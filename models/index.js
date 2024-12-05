f0dc5c422b0982a8c428bea79c295898f4f73e33
DexSus <insomniadex@gmail.com>
2024-12-05 13:42:54 +0200

    DB + docker

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('inventory_management', 'postgres', 'root', {
-  host: 'localhost',
+const sequelize = new Sequelize('inv_manager', 'postgres', 'postgres', {
+  host: '206.189.52.50',
+  port: '5002',
   dialect: 'postgres',
 });
 
